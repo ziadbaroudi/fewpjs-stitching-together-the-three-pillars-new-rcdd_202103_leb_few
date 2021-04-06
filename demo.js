0c418e705e4775b11cb50857d76ed796dbeb8021
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-06 20:59:33 +0000

    Automatically backed up by Learn

--- demo.js
+++ demo.js
@@ -25,9 +25,9 @@ function likeCallback(e) {
       // mimic Pillar 3 (Server Communication) to only update the screen if the
       // sending of information to the server succeeds.
       alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
       alert("Something went wrong!");
